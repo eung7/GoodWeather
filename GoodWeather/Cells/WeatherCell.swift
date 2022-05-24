6c49bd129f5dd0f603359788447fa35304970364
eung7 <wannabeeung@gmail.com>
2022-05-24 21:37:00 +0900

    [Add] Displaying Hard-Coded Weather into TableView

---
+++ GoodWeather/Cells/WeatherCell.swift
@@ -0,0 +1,45 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by 김응철 on 2022/05/24.
+//
+
+import UIKit
+import SnapKit
+
+class WeatherCell: UITableViewCell {
+    static let identifier = "WeatherCell"
+    
+    let cityNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Incheon"
+        label.font = .systemFont(ofSize: 22.0)
+        
+        return label
+    }()
+    
+    let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 32.0)
+        label.textColor = .systemGray
+        label.text = "23°"
+        
+        return label
+    }()
+    
+    func setupUI() {
+        [ cityNameLabel, temperatureLabel ]
+            .forEach { contentView.addSubview($0) }
+        
+        cityNameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.top.leading.equalToSuperview().inset(16)
+        }
+        
+        temperatureLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.top.trailing.equalToSuperview().inset(16)
+        }
+    }
+}
--- GoodWeather/Controllers/WeatherListViewController.swift
+++ GoodWeather/Controllers/WeatherListViewController.swift
@@ -6,8 +6,18 @@
 //
 
 import UIKit
+import SnapKit
 
 class WeatherListViewController: UIViewController {
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(WeatherCell.self, forCellReuseIdentifier: WeatherCell.identifier)
+        tableView.rowHeight = 100
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -29,6 +39,27 @@ class WeatherListViewController: UIViewController {
             target: self,
             action: #selector(didTapLeftBarButton)
         )
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension WeatherListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: WeatherCell.identifier,
+            for: indexPath) as? WeatherCell else { return UITableViewCell() }
+        cell.setupUI()
+        
+        return cell
     }
 }
 
